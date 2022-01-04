43caa09f987d120e2dc6bab47cac299e014de66c
rolla-cluster <96596250+rolla-cluster@users.noreply.github.com>
2022-01-03 19:39:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,30 @@ import sys
 
 def main():
 	print(">>> Starting ctf-starter...")
-	#IP = input("Enter the IP address of this mf box:")
 	IP = sys.argv[1]
-	if len(sys.argv) > 2:
+	if len(sys.argv) > 2 or len(sys.argv) == 0:
 		print(">>>too many arguments, quitting")
 		exit()
 	try:
-		r = requests.get(f'http://{IP}')
+		r = requests.get(f'http://{IP}') # assuming HTTP, as is standard for most boxes
 	
 		if r.status_code == 200:
 			print(">>> Found a HTTP server, diggin for a wordlist now...")
-			soup = BeautifulSoup(r.content, 'html.parser')
-			text = soup.get_text().strip()
-			f = open(f"{IP}words.txt", "x")
-			f.write(text)
-			f.close()
-			print(">>> created a text file!")
+			gather_wordlist(r)
+			
+		else:
+			print(f"Non-200 response: {r.status_code}")
 	except:
 		print(">>> No HTTP web server found")
 
 
-
+def gather_wordlist(r):
+	soup = BeautifulSoup(r.content, 'html.parser')
+			text = soup.get_text().strip()
+			f = open(f"{IP}_words.txt", "x")
+			f.write(text)
+			f.close()
+			print(">>> created a text file!")
 
 
 if __name__ == '__main__':
